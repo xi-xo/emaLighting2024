5c9c5d65d2ffd2dc57636953fbe18c86e089f840
xi-xo <jose_aguiar@hotmail.co.uk>
2024-03-10 08:41:17 +0000

    Intial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let navOpen = false;
+    let menuToggle = document.getElementById('menu-toggle');
+    let hamBox = document.getElementById('hamBox');
+    const menuLinks = document.querySelectorAll('.menu-link');
+    let navOverlay = document.getElementById('nav-overlay');
+
+    menuToggle.addEventListener('click', function() {
+        navOpen = !navOpen;
+        toggleNavVisibility(navOpen);
+    });
+
+    function toggleNavVisibility(navOpen) {
+        hamBox.classList.toggle('hamBoxOpen', navOpen);
+        updateOverlay(navOpen);
+        toggleMenuLinksDelayed(navOpen);
+    }
+
+    
+    function toggleMenuLinksDelayed(navOpen) {
+        if (navOpen) {
+            setTimeout(() => {
+                toggleMenuLink(navOpen)
+            }, 1350);
+        } else {
+            toggleMenuLink(navOpen)
+        }
+        
+    }
+    function toggleMenuLink(navOpen) {
+        menuLinks.forEach((link) => {
+            link.style.display = navOpen ? 'block' : 'none';
+        })
+    }
+
+    function updateOverlay(navOpen) {
+        if (navOpen) {
+            navOverlay.style.top = '0';
+        } else {
+            navOverlay.style.top = '-100%';
+        }
+    }
+});
